84138642dd15cf01e6affedd83a6e1f4a7e454e1
/home/memosk8 <memolopez9363@gmail.com>
2022-11-03 14:21:42 -0600

    Added functions frankenSplice() and bouncer()

--- js_playground.js
+++ js_playground.js
@@ -1275,3 +1275,34 @@ function titleCase(str) {
 }
 
 console.log(titleCase("I'm a little tea pot"))
+
+/**
+  Copy each element of the first array into the second array, in order.
+  Begin inserting elements at index n of the second array.
+  Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+/* function frankenSplice(arr1, arr2, n) {
+  return [...arr2.slice(0,n), ...arr1, ...arr2.slice(n)];
+} */
+
+const frankenSplice = (arr1,arr2,n) =>
+[...arr2.slice(0,n), ...arr1, ...arr2.slice(n)];
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1))
+
+/**
+  return <new array> that only inlcudes truthy values from <given array>
+  without mutating the original array
+*/
+
+function bouncer(arr) {
+  let truthies = [];
+  arr.forEach((el) => {
+    if (!!el)
+      truthies.push(el)
+  });
+  return truthies;
+}
+
+console.log(bouncer([7, "ate", "", false, NaN, undefined,9]))
