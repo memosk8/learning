7aa0a1947766e199f2b6d1842eb2dd15cf3d93e1
/home/memosk8 <memolopez9363@gmail.com>
2022-11-07 19:51:18 -0600

    added Functions: diffArray(), destroyer()

--- js_playground.js
+++ js_playground.js
@@ -976,10 +976,9 @@ noisy(Math.min)(3.3, 2.3, 1.4);
 
 function filter(array, test) {
   let passed = [];
-  for (let element of array) {
+  for (let element of array)
     if (test(element))
       passed.push(element);
-  }
   return passed;
 }
 
@@ -1760,11 +1759,35 @@ function add_(x) {
 console.log(add_(10)(20)(30));
 
 function sumAll(arr) {
-  arr.sort((a,b) => a - b)
+  arr.sort((a, b) => a - b)
   let nums = [];
-  for(let i = arr[0]; i <= arr[1]; i++)
+  for (let i = arr[0]; i <= arr[1]; i++)
     nums.push(i)
   return nums.reduce((prev, curr) => prev + curr);
 }
 
-console.log(sumAll([10, 5]))
+console.log(sumAll([10, 12]))
+
+/** 
+ * Compare two arrays and return a new array
+ * with any items only found in one of the two given arrays, but not both. 
+ * In other words, return the symmetric difference of the two arrays
+ */
+
+function diffArray(arr1, arr2) {
+  return [...arr1, ...arr2].filter(
+    n => arr1.indexOf(n) < 0 || arr2.indexOf(n) < 0
+  );
+}
+
+console.log(diffArray([1, 2, 3, 5, 8], [1, 2, 3, 4, 5, 22]))
+
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
+
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
+
+function destroyer(arr, ...valsToRemove) {
+  return arr.filter(elem => !valsToRemove.includes(elem));
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3))
