abc093a30908616937c87140441d2228517f76bf
/home/memosk8 <memolopez9363@gmail.com>
2022-11-15 14:13:26 -0600

    added binaryAgent(str)  to convert binary to str

--- js_playground.js
+++ js_playground.js
@@ -2003,4 +2003,16 @@ function steamrollArray(arr) {
   }
   return flattenedArray;
 };
+
 steamrollArray([1, [2], [3, [[4]]]]);
+
+function binaryAgent(str) {
+  const binaryParts = str.split(" ");
+  const decimals = binaryParts.map(n => parseInt(n, 2));
+  const chars = decimals.map(n => String.fromCharCode(n))
+  const string = chars.join("")
+  console.log(string)
+  return string;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
