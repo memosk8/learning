438bf4224902b501943043e35af0bb64a820f172
/home/memosk8 <memolopez9363@gmail.com>
2022-11-15 14:02:53 -0600

    function steamrollArray(arr) flatten nested arrays

--- js_playground.js
+++ js_playground.js
@@ -1989,10 +1989,18 @@ console.log(dropElements([1, 2, 3, 4], function (n) { return n > 4 }))
 console.log(dropElements([1, 2, 3], function (n) { return n < 3; }));
 
 function steamrollArray(arr) {
-  let flat = []
-
-
-  return arr;
-}
-
+  const flattenedArray = [];
+  // Loop over array contents
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      // Recursively flatten entries that are arrays
+      //  and push into the flattenedArray
+      flattenedArray.push(...steamrollArray(arr[i]));
+    } else {
+      // Copy contents that are not arrays
+      flattenedArray.push(arr[i]);
+    }
+  }
+  return flattenedArray;
+};
 steamrollArray([1, [2], [3, [[4]]]]);
